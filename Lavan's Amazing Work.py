1e4ccb3f21a5023675a4799b1360763b01173796
yourfavouritefrenchfry <44682901+yourfavouritefrenchfry@users.noreply.github.com>
2018-11-01 15:47:42 -0400

    Add files via upload

---
+++ Lavan's Amazing Work.py
@@ -0,0 +1,10 @@
+Lavan#
+def every_other_new (list):
+    """
+    Return the list with every other element of the list starting with the first
+    >>>every_other_new([5, "book", "computer", 4])
+    [5, "computer"]
+    """
+    return list[::2]
+
+    
